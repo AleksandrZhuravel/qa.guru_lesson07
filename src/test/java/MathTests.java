bd3832480658e985829b278780817bb6090342c2
AleksandrZhuravel <cah40c@tut.by>
2020-08-23 19:14:12 +0300

    Tests added

--- src/test/java/MathTests.java
+++ src/test/java/MathTests.java
@@ -1,2 +1,18 @@
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Tag("easy_tests")
 public class MathTests {
+
+    @Test
+    void positiveTest() {
+        int a = 8;
+        String b = System.getProperty("b");
+        int c = Integer.parseInt(b);
+        System.out.println(a / c);
+        assertEquals(a / c, 2);
+    }
+
 }
